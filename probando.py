b63b21153a684b5fba2d4b26990793e7a34900b8
PennyGomball DIAZGALVEZ <salvador.diaz@usach.cl>
2025-04-28 15:41:45 +0000

    act

---
+++ probando.py
@@ -0,0 +1,14 @@
+# Importo librerias necesarias
+from pygwalker.api.streamlit import StreamlitRenderer
+import pandas as pd 
+import streamlit as st
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+
+    # Can be used wherever a "file-like" object is accepted:
+mdataframe = pd.read_csv(uploaded_file,sep=";")
+datos_pyg = StreamlitRenderer(mdataframe)
+datos_pyg.explorer()
+
+    
